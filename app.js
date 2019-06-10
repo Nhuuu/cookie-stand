5773ff7485767883933a25a57671ccfdeec86919
Nhu Trinh <nqtrinh87@gmail.com>
2019-06-10 16:34:18 -0700

    generating avg cookies sold and total cookies to bake per day for each store

--- app.js
+++ app.js
@@ -1,65 +1,6 @@
 'use strict';
 
-// Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
-
-var stores = ['firstAndPike', 'seaTacAirport', 'seattleCenter', 'capitolHill', 'alki'];
-
-var firstAndPike = {
-  minHourCustomer: 23,
-  maxHourCustomer: 65,
-  avgCookieSold: 6.3,
-  randHourlyCustomer: function(){
-    return Math.floor(Math.random() * this.maxHourCustomer);
-  },
-  projectedCookiesSoldHourly: []
-}
-
-var seaTacAirport = {
-  minHourCustomer: 3,
-  maxHourCustomer: 24,
-  avgCookieSold: 1.2,
-  randHourlyCustomer: function(){
-    return Math.floor(Math.random() * this.maxHourCustomer);
-  },
-  projectedCookiesSoldHourly: []
-}
-
-var seattleCenter = {
-  minHourCustomer: 11,
-  maxHourCustomer: 38,
-  avgCookieSold: 3.7,
-  randHourlyCustomer: function(){
-    return Math.floor(Math.random() * this.maxHourCustomer);
-  },
-  projectedCookiesSoldHourly: []
-}
-
-var capitolHill = {
-  minHourCustomer: 20,
-  maxHourCustomer: 38,
-  avgCookieSold: 2.3,
-  randHourlyCustomer: function(){
-    return Math.floor(Math.random() * this.maxHourCustomer);
-  },
-  projectedCookiesSoldHourly: []
-}
-
-var alki = {
-  location: '1st and Pike',
-  minHourCustomer: 2,
-  maxHourCustomer: 16,
-  avgCookieSold: 4.6,
-  randHourlyCustomer: function(){
-    return Math.floor(Math.random() * this.maxHourCustomer);
-  },
-  cookiesSoldHourly: []
-}
-
-var projectedSales = function(){
-  // for each on storeArr push 
-  return avgCookieSold * randHourlyCustomer();
-}
-// for each store, create a list element and display on sales.html
+// Open store hours
 
 var hours = [
   '6am',
@@ -79,9 +20,120 @@ var hours = [
   '8pm'
 ]
 
-// for each hour, random generate projectedSales
+// Each store object
+
+var firstAndPike = {
+  minHourCustomer: 23,
+  maxHourCustomer: 65,
+  avgCookieSold: 6.3,
+  randHourlyCustomer: function(){
+    return Math.floor(Math.random() * (this.maxHourCustomer - this.minHourCustomer + 1) + this.minHourCustomer);
+  },
+  cookiesSoldHourly: [],
+  totalCookies: 0
+}
+
+var seaTacAirport = {
+  minHourCustomer: 3,
+  maxHourCustomer: 24,
+  avgCookieSold: 1.2,
+  randHourlyCustomer: function(){
+    return Math.floor(Math.random() * (this.maxHourCustomer - this.minHourCustomer + 1) + this.minHourCustomer);
+  },
+  cookiesSoldHourly: [],
+  totalCookies: 0
+}
+
+var seattleCenter = {
+  minHourCustomer: 11,
+  maxHourCustomer: 38,
+  avgCookieSold: 3.7,
+  randHourlyCustomer: function(){
+    return Math.floor(Math.random() * (this.maxHourCustomer - this.minHourCustomer + 1) + this.minHourCustomer);
+  },
+  cookiesSoldHourly: [],
+  totalCookies: 0
+}
+
+var capitolHill = {
+  minHourCustomer: 20,
+  maxHourCustomer: 38,
+  avgCookieSold: 2.3,
+  randHourlyCustomer: function(){
+    return Math.floor(Math.random() * (this.maxHourCustomer - this.minHourCustomer + 1) + this.minHourCustomer);
+  },
+  cookiesSoldHourly: [],
+  totalCookies: 0
+}
+
+var alki = {
+  minHourCustomer: 2,
+  maxHourCustomer: 16,
+  avgCookieSold: 4.6,
+  randHourlyCustomer: function(){
+    return Math.floor(Math.random() * (this.maxHourCustomer - this.minHourCustomer + 1) + this.minHourCustomer);
+  },
+  cookiesSoldHourly: [],
+  totalCookies: 0
+}
 
 
-// total function to add all in the array for each location
+// For each store, for each hour calculate avg cookies sold and total cookies needed to be baked daily.
 
-// Calculating the sum of these hourly totals; your output for each location...
+var salesFirstAndPike = function(){
+  for(var i = 0; i < hours.length; i++){
+    var hourlySales = Math.round(firstAndPike.avgCookieSold * firstAndPike.randHourlyCustomer());
+    firstAndPike.cookiesSoldHourly.push(hourlySales)
+    firstAndPike.totalCookies = firstAndPike.totalCookies + hourlySales;
+  }
+}
+
+salesFirstAndPike();
+
+
+var salesSeaTacAirport = function(){
+  for(var i = 0; i < hours.length; i++){
+    var hourlySales = Math.round(firstAndPike.avgCookieSold * firstAndPike.randHourlyCustomer());
+    firstAndPike.cookiesSoldHourly.push(hourlySales)
+    firstAndPike.totalCookies = firstAndPike.totalCookies + hourlySales;
+  }
+}
+
+salesSeaTacAirport();
+
+
+var salesSeattleCenter = function(){
+  for(var i = 0; i < hours.length; i++){
+    var hourlySales = Math.round(firstAndPike.avgCookieSold * firstAndPike.randHourlyCustomer());
+    firstAndPike.cookiesSoldHourly.push(hourlySales)
+    firstAndPike.totalCookies = firstAndPike.totalCookies + hourlySales;
+  }
+}
+
+salesSeattleCenter();
+
+
+var salesCapitolHill = function(){
+  for(var i = 0; i < hours.length; i++){
+    var hourlySales = Math.round(firstAndPike.avgCookieSold * firstAndPike.randHourlyCustomer());
+    firstAndPike.cookiesSoldHourly.push(hourlySales)
+    firstAndPike.totalCookies = firstAndPike.totalCookies + hourlySales;
+  }
+}
+
+salesCapitolHill();
+
+
+var salesAlki = function(){
+  for(var i = 0; i < hours.length; i++){
+    var hourlySales = Math.round(firstAndPike.avgCookieSold * firstAndPike.randHourlyCustomer());
+    firstAndPike.cookiesSoldHourly.push(hourlySales)
+    firstAndPike.totalCookies = firstAndPike.totalCookies + hourlySales;
+  }
+}
+
+salesAlki();
+
+
+// Add info to the page!
+
